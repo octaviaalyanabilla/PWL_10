ae196135ecce9eaae0655e75a61dea8152fd12c1
ACER\Z1402 <octaviaalya01@gmail.com>
2021-03-27 10:46:10 +0700

    tugas menambah email dan tanggal lahir

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,26 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail; 
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
-use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable; 
-use Illuminate\Notifications\Notifiable; 
-use Illuminate\Database\Eloquent\Model; //Model Eloquent
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    protected $table="mahasiswas"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
-    public $timestamps= false;
-    protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+    use HasFactory;
 
-    /** * The attributes that are mass assignable.
-* * @var array */
-protected $fillable = [ 
-    'Nim', 
-    'Nama', 
-    'Kelas', 
-    'Jurusan', 
-    'No_Handphone', 
-    ]; 
-};
+    protected $table='mahasiswa';
+    protected $primaryKey = 'nim';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_handphone',
+        // menambahkan kolom
+        'email',
+        'tgl_lahir'
+    ];
+}
 
