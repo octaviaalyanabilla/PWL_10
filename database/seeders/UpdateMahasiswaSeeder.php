f392f5ec9b68f8a7b257448f06c89074c652939f
ACER\Z1402 <octaviaalya01@gmail.com>
2021-04-11 21:53:27 +0700

    update data mahasiswa di kolom kelas_id

---
+++ database/seeders/UpdateMahasiswaSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+class UpdateMahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //misal kita update data mahasiswa yang ada saat ini milik TI 2A
+        DB::table('mahasiswas')->update(['kelas_id' => 1]);
+    }
+}
