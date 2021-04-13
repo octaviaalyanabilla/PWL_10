39e79b7a6013d579c7a3032fea6f68c9d68e0b43
ACER\Z1402 <octaviaalya01@gmail.com>
2021-04-13 15:07:35 +0700

    Mengisi controller di bagian fungsi store dan create

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -22,9 +22,10 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        return view('articles.create');
+        
     }
 
     /**
@@ -35,7 +36,17 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if($request->file('image')){
+            $image_name = $equest->file('image')->store('images', 'public');
+
+        }
+
+        Article::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'featured_image' => $image_name,
+        ]);
+        return 'Artikel berhasil disimpan';
     }
 
     /**
