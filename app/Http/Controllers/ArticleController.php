78db36caa69a87f0e99a3921d00850c8d06d8bbc
ACER\Z1402 <octaviaalya01@gmail.com>
2021-04-13 15:41:32 +0700

    Menambahkan function pada edit dan update di controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -37,7 +37,7 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
         if($request->file('image')){
-            $image_name = $equest->file('image')->store('images', 'public');
+            $image_name = $request->file('image')->store('images', 'public');
 
         }
 
@@ -66,9 +66,11 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function edit(Article $article)
+    public function edit($id)
     {
-        //
+        $article = Article::find($id);
+
+        return view('articles.edit', ['article' => $article]);
     }
 
     /**
@@ -78,9 +80,21 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Article $article)
+    public function update(Request $request, $id)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if ($article->featured_image && file_exists(storage_path('app/public/' . $article->featured_image))) {
+            \Storage::delete('public/' . $article->featured_image);
+        }
+        $image_name = $request->file('image')->store('image', 'public');
+        $article->featured_image = $image_name;
+
+        $article->save();
+        return 'Artikel berhasil diubah';
     }
 
     /**
